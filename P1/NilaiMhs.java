26015978efed4b8a85ff76fd0cbf130a26be5f41
fathirzahran17-hue <fathirzahran17@gmail.com>
2026-04-21 20:36:09 +0700

    Add files via upload

--- P1/NilaiMhs.java
+++ P1/NilaiMhs.java
@@ -1,1 +1,42 @@
+package P1;
 
+import java.util.Scanner;
+
+/**
+ *
+ * @author fathir
+ */
+public class NilaiMhs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nim, nama, grade;
+        double uts, uas, rata;
+        
+        System.out.println("Data Mahasiswa: ");
+        System.out.print("NIM      : "); nim = input.next();
+        System.out.print("Nama     : "); nama = input.next();
+        System.out.print("Nilai UTS: "); uts = input.nextDouble();
+        System.out.print("Nilai UAS: "); uas = input.nextDouble();
+        
+        rata = (uts + uas) / 2;
+        
+        if (rata < 50)
+            grade = "E";
+        else if (rata < 60)
+            grade = "D";
+        else if (rata < 70)
+            grade = "C";
+        else if (rata < 80)
+            grade = "B";
+        else
+            grade = "A";
+
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nim\tNama\tUTS\tUAS\tRata\tGrade");
+        System.out.println("---------------------------------------------------------");
+        System.out.println(nim + "\t" + nama + "\t" + uts + "\t" + uas + "\t" + rata + "\t" + grade);
+
+        System.out.println("");
+        System.out.println("");
+    } 
+}
