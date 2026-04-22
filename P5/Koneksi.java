658c7623b1946cd7bcb3a12d2c9927e9151e526c
fathirzahran17-hue <fathirzahran17@gmail.com>
2026-04-22 22:09:51 +0700

    Add files via upload

---
+++ P5/Koneksi.java
@@ -0,0 +1,25 @@
+package pert5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Koneksi {
+    public static Connection getKoneksi() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/db_mahasiswa",
+                "root",
+                ""
+            );
+
+            System.out.println("Koneksi berhasil");
+
+        } catch (Exception e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+        }
+        return conn;
+    }
+}
